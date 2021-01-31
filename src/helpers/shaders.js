fc94bcf85b730c24b47a0491996a68c1a7ec5763
Alexandre Di Guida <alexandre.diguida@gmail.com>
2021-01-31 17:15:51 +0000

    Add cosPalette function

--- src/helpers/shaders.js
+++ src/helpers/shaders.js
@@ -153,14 +153,25 @@ export const vertexShader = `
 `
 
 export const fragmentShader = `
+  varying vec2 vUv;
   varying float vDistort;
 
   uniform float uTime;
   uniform float uIntensity;
 
+  vec3 cosPalette(float t, vec3 a, vec3 b, vec3 c, vec3 d) {
+    return a + b * cos(9.28318 * (c * t + d));
+  }
+
   void main() {
     float distort = vDistort * uIntensity;
-    vec3 color = vec3(distort);
+
+    vec3 brightness = vec3(0.5, 0.5, 0.5);
+    vec3 contrast = vec3(0.5, 0.5, 0.5);
+    vec3 oscilation = vec3(1.0, 1.0, 1.0);
+    vec3 phase = vec3(0.0, 0.1, 0.2);
+
+    vec3 color = cosPalette(distort, brightness, contrast, oscilation, phase);
 
     gl_FragColor = vec4(color, 1.0);
   }
